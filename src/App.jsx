56584e856a564d15f8386f9f94c7a2a09101cfb5
YutanovMaksim <yutanovm@yandex.ru>
2022-10-18 17:30:13 +0300

    homework1 v.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,15 @@
-import { Form } from './components/Form/Form'
-import './index.css'
-import { Count } from './components/Count'
-import { Child } from './components/Child'
-import { Form as FormClass } from './class-components/Form'
-import { Count as CountClass } from './class-components/Count'
-import { useState } from 'react'
+import { Message as FuncMessage } from './FuncComponent/Message/Message'
+import { Message as ClassMessage } from './ClassComponent/Message/Message'
 
 export const App = () => {
-  const [name, setName] = useState('geek')
-  const [count, setCount] = useState(0)
-  const arr = ['ivanov', 'petrov', 'sidorov']
-
-  const handleChangeName = (ev) => {
-    setName(ev.target.value)
-  }
+  const [name, setName] = 'wqeqe'
   return (
-    <div className="App">
-      <h2 style={{ backgroundColor: 'green' }}>Func Component</h2>
-      <CountClass count={10} />
+    <>
+      <ClassMessage text={name} />
+      <ClassInput changeName={setName} />
       <hr />
-      <FormClass />
-      <h2>Class Component</h2>
-      <Count name="geekbrains" />
-      <hr />
-      <h3>Parent component</h3>
-      <p>{count}</p>
-      <input onChange={handleChangeName} />
-      <h3>Child component</h3>
-      <Child name={name} handleChangeCount={setCount} />
-      {arr.map((el, idx) => {
-        return <div key={idx}>{el}</div>
-      })}
-      <Form />
-    </div>
+      <FuncMessage text={name} />
+      <FuncInput changeName={setName} />
+    </>
   )
 }
