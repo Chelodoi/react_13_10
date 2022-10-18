3723526143ace192e9219661035ccf443ed9f50c
YutanovMaksim <yutanovm@yandex.ru>
2022-10-18 19:29:59 +0300

    homework1 v.3

--- src/ClassComponent/Input/Input.jsx
+++ src/ClassComponent/Input/Input.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
 export class Input extends React.Component {
-  constructor(props) {
-    super(props)
-    this.changeInput((ev) => {
-      this.props.setNameClass({ nameClass: ev.target.value })
-    })
+  changeInput = (ev) => {
+    this.props.changeName(ev.target.value)
   }
+
   render() {
     return <input type="text" onChange={this.changeInput} />
   }
